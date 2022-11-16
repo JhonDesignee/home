830ae2ae6394ee86561d19c1e337a62cfe74d689
JhonDesignee <jhonata.oliveira.275@gmail.com>
2022-11-16 00:59:49 -0300

    update

--- pages/projects/gerador-de-link-whatsapp.vue
+++ pages/projects/gerador-de-link-whatsapp.vue
@@ -3,8 +3,8 @@
     <v-card color="primary" class="rounded-lg pt-2">
       <v-container>
         <v-form ref="form">
-          <v-text-field outlined no-resize type="number" v-model="input_phone_number" :rules="[textFieldRule]" color="accent" class="rounded-lg" label="Número de telefone"></v-text-field>
-          <v-textarea outlined no-resize v-model="input_message" class="rounded-lg" color="accent" label="Mensagem (opcional)"></v-textarea> 
+          <v-text-field outlined no-resize type="number" v-model="input_phone_number" :rules="[textFieldRule]" color="accent" class="rounded-lg" label="Número de telefone" hint="formato internacional" persistent-hint></v-text-field>
+          <v-textarea outlined no-resize v-model="input_message" class="rounded-lg" color="accent" label="Mensagem" hint="opcional" persistent-hint></v-textarea> 
           <v-btn block class="rounded-lg" color="accent" @click="generateWhatsAppLink()">Gerar</v-btn>
         </v-form>
         <v-textarea outlined no-resize readonly :append-icon="clipboard_icon" v-model="output_link" color="accent" @click:append="copyToClipboard(output_link, clipboard_callback)" class="mt-6 rounded-lg"></v-textarea>
@@ -64,8 +64,8 @@
       title: "Gerador de link WhatsApp", 
       meta: [
          { hid: "og:title", name: "og:title", content: "Gerador de link WhatsApp" }, 
-         { hid: "description", name: "description", content: "Random things" },
-         { hid: "og:description", name: "og:description", content: "Random things" },
+         { hid: "description", name: "description", content: "Gere o link do seu contato do WhatsApp" },
+         { hid: "og:description", name: "og:description", content: "Gere o link do seu contato do WhatsApp" },
          { hid: "og:url", name: "og:url", content: "https://jhondesignee.netlify.app/projects/gerador-de-link-whatsapp" }
       ]
     }
